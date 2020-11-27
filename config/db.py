af8d460f189e7ad4226fd21a1fcd5f97d5d9e286
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-11-27 10:52:38 +0100

    Add DB settings.

---
+++ config/db.py
@@ -0,0 +1,14 @@
+from config.settings import get_settings
+
+settings = get_settings()
+
+DB_CONFIG = {
+    "connections": {
+        "default": settings.DATABASE_URL
+    },
+    "apps": {
+        "models": {
+            "models": settings.MODELS
+        }
+    }
+}
