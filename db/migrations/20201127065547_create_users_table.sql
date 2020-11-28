bf40c2d4dd8ca2d78383174564f4085fd8deefad
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-11-28 20:06:05 +0100

    Add confirmation field to users table.

--- db/migrations/20201127065547_create_users_table.sql
+++ db/migrations/20201127065547_create_users_table.sql
@@ -3,7 +3,8 @@ CREATE TABLE users (
     id UUID PRIMARY KEY,
     email varchar(255) UNIQUE NOT NULL,
     hashed_password varchar(255) NOT NULL,
-    is_active boolean NOT NULL DEFAULT FALSE
+    is_active boolean NOT NULL DEFAULT FALSE,
+    confirmation UUID
 )
 -- migrate:down
 DROP TABLE users
