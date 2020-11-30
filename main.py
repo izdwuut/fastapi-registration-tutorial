456dfc61dd6f93d9355b01702f48aa4c0a1d60c8
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-11-30 08:55:34 +0100

    Add auth router.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tortoise.contrib.fastapi import register_tortoise
 from config.db import DB_CONFIG
 from config.settings import Settings
 import uvicorn
-
+from routers.auth import auth_router
 settings = Settings()
 app = FastAPI(title=settings.APP_NAME)
 
@@ -13,6 +13,11 @@ register_tortoise(
     generate_schemas=False,
 )
 
+app.include_router(
+    auth_router,
+    prefix=settings.API_PREFIX + '/auth',
+    tags=['Authentication'],
+)
 
 if __name__ == '__main__':
     uvicorn.run(app, host=settings.HOST, port=settings.PORT)
---
+++ routers/auth.py
@@ -0,0 +1,50 @@
+from fastapi import APIRouter
+from fastapi import Depends, HTTPException, status
+from services.auth import Auth
+from services.mailer import Mailer
+from config.settings import Settings
+from models import users
+from jose import jwt
+
+settings = Settings()
+auth_router = APIRouter()
+
+
+@auth_router.post("/register")
+async def register(form_data: users.CreateUser = Depends()):
+    if await users.UserModel.get_or_none(email=form_data.email) is not None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User already exists"
+        )
+    user = await users.UserModel.create(
+        email=form_data.email,
+        hashed_password=Auth.get_password_hash(
+            form_data.password.get_secret_value()
+        )
+    )
+    confirmation = Auth.get_confirmation_token(user.id)
+    user.confirmation = confirmation["jti"]
+    await user.save()
+    Mailer.send_confirmation_message(confirmation["token"], form_data.email)
+
+
+@auth_router.get("/verify/{token}")
+async def verify(token: str):
+    invalid_token_error = HTTPException(status_code=400, detail="Invalid token")
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=settings.TOKEN_ALGORITHM)
+    except jwt.JWTError:
+        raise HTTPException(status_code=403, detail="Token has expired")
+    if payload['scope'] != 'registration':
+        raise invalid_token_error
+    user = await users.UserModel.get_or_none(id=payload['sub'])
+    if not user or str(user.confirmation) != payload['jti']:
+        raise invalid_token_error
+    if user.is_active:
+        raise HTTPException(status_code=403, detail="User already activated")
+    user.confirmation = None
+    user.is_active = True
+    await user.save()
+    return await users.User_Pydantic.from_tortoise_orm(user)
+
